eca6ccab69c052babd93a6b2858e08c9561e7302
Pharanyu <pharanyuchuenjit@gmail.com>
2022-05-29 20:33:03 +0700

    7th commit

--- script/script.js
+++ script/script.js
@@ -2,11 +2,11 @@
 
 var body = document.querySelector(".body");
 
-var editBtn = body.querySelector(".button_edit");
+var editBtn = body.querySelector(".button__edit");
 
 var edit = body.querySelector(".edit") 
 
-var closeBtn = edit.querySelector(".edit__close")
+var closeBtn = edit.querySelector(".button__closed")
 
 var overlay = body.querySelector(".overlay")
 
@@ -55,42 +55,31 @@ function handleProfileFormSubmit(evt) {
 // it will watch the submit event
 profileFormElement.addEventListener('submit', handleProfileFormSubmit);     
 
-// render card element fields
+// render card element fields (prepare for next project)
+// var data = {
+//     name: ['Yoesmite Valley', 'Lake Louise', 'Bald mountains', 'Latemar', 'Vanoise National Park', 'Lago di Braies'],
+//     link: ['https://code.s3.yandex.net/web-code/yosemite.jpg',
+//         'https://code.s3.yandex.net/web-code/lake-louise.jpg',
+//         'https://code.s3.yandex.net/web-code/bald-mountains.jpg',
+//         'https://code.s3.yandex.net/web-code/latemar.jpg',
+//         "https://code.s3.yandex.net/web-code/vanoise.jpg",
+//         'https://code.s3.yandex.net/web-code/lago.jpg']
+// }
 
-var data = {
-    name: ['Yoesmite Valley', 'Lake Louise', 'Bald mountains', 'Latemar', 'Vanoise National Park', 'Lago di Braies'],
-    link: ['https://code.s3.yandex.net/web-code/yosemite.jpg',
-        'https://code.s3.yandex.net/web-code/lake-louise.jpg',
-        'https://code.s3.yandex.net/web-code/bald-mountains.jpg',
-        'https://code.s3.yandex.net/web-code/latemar.jpg',
-        "https://code.s3.yandex.net/web-code/vanoise.jpg",
-        'https://code.s3.yandex.net/web-code/lago.jpg']
-}
-
-function getCardElement(data) {
+// function getCardElement(data) {
     
-    var elements = document.querySelector(".elements")
-    for(let i = 0; i < data.name.length; i++)
-    {
-        elements.insertAdjacentHTML("beforeend", `
-        <div class="element">
-        <img src=${data.link[i]} alt=${data.name[i]} class="element__pic">
-        <div class="element__name">
-            <p class="element__title">${data.name[i]}</p>
-            <img src="./images/icon.svg" alt="icon" class="element__icon">
-        </div>
-    </div>`)
-    }
-}
-getCardElement(data);
-
-
-
-// `
+//     var elements = document.querySelector(".elements")
+//     for(let i = 0; i < data.name.length; i++)
+//     {
+//         elements.insertAdjacentHTML("beforeend", `
 //         <div class="element">
-//             <img src=${data.link[i]} alt=${data.name[i]} class="element__pic">
-//             <div class="element__name">
-//                 <p class="element__title">${data.name[i]}</p>
-//                 <img src="./images/icon.svg" alt="icon" class="element__icon">
-//             </div>
-//         </div>`
+//         <img src=${data.link[i]} alt=${data.name[i]} class="element__pic">
+//         <div class="element__name">
+//             <p class="element__title">${data.name[i]}</p>
+//             <img src="./images/icon.svg" alt="icon" class="element__icon">
+//         </div>
+//     </div>`)
+//     }
+// }
+
+
