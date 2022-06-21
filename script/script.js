14f091d4a5ea2b66e21a9d7037d3e99e22aa0a49
Pharanyu <pharanyuchuenjit@gmail.com>
2022-06-21 15:20:12 +0100

    feature: fix popup function

--- script/script.js
+++ script/script.js
@@ -1,70 +1,58 @@
 const body = document.querySelector('.body');
 const editBtn = body.querySelector('.profile__button-edit');
-const edit = body.querySelector('.edit');
-const profileCloseBtn = edit.querySelector('.edit__button-closed');
+
 const elements = document.querySelector('.elements');
 
-// add form part
-const add = body.querySelector('.add');
-const addButton = body.querySelector('.profile__button');
-const addCloseBtn = body.querySelector('.add__button-closed');
-const addFormElement = add.querySelector('.add__form');
-const titleInput = body.querySelector('.add__input_type_title');
-const linkInput = body.querySelector('.add__input_type_link');
+const popup = document.querySelector('.popup');
+
+const addBtn = body.querySelector('.profile__button');
 
 const elementTemplate = document.querySelector('.element-template').content;
-
-// pic variable
-const pic = document.querySelector('.pic');
-const picBtn = document.querySelector('.pic__button-closed');
-const picImg = document.querySelector('.pic__img');
-const picTitle = document.querySelector('.pic__title');
+const editTemplate = document.querySelector('.edit-template').content;
+const addTemplate = document.querySelector('.add-template').content;
+const picTemplate = document.querySelector('.pic-template').content;
 
 //  work with profile form
-const showForm = () => {
-  edit.classList.remove('edit_closed');
-
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
+const showPopup = () => {
+  popup.classList.remove('popup_closed');
 };
 
-const closeForm = () => {
-  edit.classList.add('edit_closed');
+const closePopup = () => {
+  popup.classList.add('popup_closed');
+  popup.removeChild(popup.lastChild);
 };
-
-// work with adding form
-const showAddForm = () => {
-  add.classList.remove('add_closed');
-};
-
-const closeAddForm = () => {
-  add.classList.add('add_closed');
-};
-
-addButton.addEventListener('click', showAddForm);
-addCloseBtn.addEventListener('click', closeAddForm);
-
-// find the form in the DOM
-const profileFormElement = edit.querySelector('.edit__form');
-
-// find the form fields in the DOM
-const nameInput = profileFormElement.querySelector('.edit__input_type_name');
-const jobInput = profileFormElement.querySelector('.edit__input_type_career');
 
 // find the form fields in the DOM
 const profileName = body.querySelector('.profile__info-name');
 const profileJob = body.querySelector('.profile__info-career');
 
-// the form submission handler. Note that its name
-// starts with a verb and concisely describes what it does
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
+// function for show edit profile form and edit profile
+const showProfileForm = () => {
+  const edit = editTemplate.querySelector('.edit__container').cloneNode(true);
 
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
+  const editCloseBtn = edit.querySelector('.popup__button-closed');
+  editCloseBtn.addEventListener('click', closePopup);
 
-  closeForm();
-}
+  popup.append(edit);
+  showPopup();
+  const inputName = document.querySelector('.edit__input_type_name');
+  const inputJob = document.querySelector('.edit__input_type_career');
+  const editForm = edit.querySelector('.edit__form');
+
+  function handleProfileFormSubmit(evt) {
+    evt.preventDefault();
+
+    profileName.textContent = inputName.value;
+    profileJob.textContent = inputJob.value;
+
+    closePopup();
+  }
+
+  editForm.addEventListener('submit', handleProfileFormSubmit);
+
+  inputName.value = profileName.textContent;
+  inputJob.value = profileJob.textContent;
+};
 
 //  render card element fields
 const data = {
@@ -88,21 +76,25 @@ const data = {
 
 // function popup picture
 const popupPicture = (evt) => {
-  pic.classList.remove('pic_closed');
+  const pic = picTemplate.querySelector('.pic__container').cloneNode(true);
+
+  const picCloseBtn = pic.querySelector('.popup__button-closed');
+  picCloseBtn.addEventListener('click', closePopup);
+
+  const picImg = pic.querySelector('.pic__img');
+  const picTitle = pic.querySelector('.pic__title');
 
   picImg.src = evt.target.src;
   picTitle.textContent = evt.target.alt;
   picImg.alt = evt.target.alt;
+
+  popup.append(pic);
+  showPopup();
 };
 
 // function delete element
 const deleteElement = (evt) => {
   evt.target.closest('.element').remove();
-};
-
-// function close picture modal
-const closePic = () => {
-  pic.classList.add('pic_closed');
 };
 
 // function for like icon on element
@@ -144,26 +136,36 @@ const getCardElement = (data) => {
 
 getCardElement(data);
 
-// function for create new element
-const handleAddFormSubmit = (evt) => {
-  evt.preventDefault();
+// function for show add form and create element
 
-  data.name.unshift(titleInput.value);
-  data.link.unshift(linkInput.value);
-  const element = createCard(data.name[0], data.link[0]);
+const showAddForm = () => {
+  const add = addTemplate.querySelector('.add__container').cloneNode(true);
 
-  elements.prepend(element);
-  titleInput.value = '';
-  linkInput.value = '';
-  closeAddForm();
+  const addCloseBtn = add.querySelector('.popup__button-closed');
+  addCloseBtn.addEventListener('click', closePopup);
+
+  popup.append(add);
+  showPopup();
+  const titleInput = document.querySelector('.add__input_type_title');
+  const linkInput = document.querySelector('.add__input_type_link');
+  const addForm = document.querySelector('.add__form');
+
+  const handleAddFormSubmit = (evt) => {
+    evt.preventDefault();
+
+    data.name.unshift(titleInput.value);
+    data.link.unshift(linkInput.value);
+    const element = createCard(data.name[0], data.link[0]);
+
+    elements.prepend(element);
+    titleInput.value = '';
+    linkInput.value = '';
+    closePopup();
+  };
+
+  addForm.addEventListener('submit', handleAddFormSubmit);
 };
 
 // connect the handler to the form:
-// it will watch the submit event
-editBtn.addEventListener('click', showForm);
-profileCloseBtn.addEventListener('click', closeForm);
-
-profileFormElement.addEventListener('submit', handleProfileFormSubmit);
-addFormElement.addEventListener('submit', handleAddFormSubmit);
-
-picBtn.addEventListener('click', closePic);
+editBtn.addEventListener('click', showProfileForm);
+addBtn.addEventListener('click', showAddForm);
