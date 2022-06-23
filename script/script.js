8cb652c30f920b8adcc982167e96f707a34779cf
Pharanyu <pharanyuchuenjit@gmail.com>
2022-06-23 01:36:34 +0100

    feature: fix popup function by not using OOP

--- script/script.js
+++ script/script.js
@@ -1,25 +1,34 @@
 const body = document.querySelector('.body');
-const editBtn = body.querySelector('.profile__button-edit');
-
 const elements = document.querySelector('.elements');
-
-const popup = document.querySelector('.popup');
-
+const elementTemplate = document.querySelector('.element-template').content;
+const editBtn = body.querySelector('.profile__button-edit');
+const closeBtn = document.querySelectorAll('.popup__button-closed');
 const addBtn = body.querySelector('.profile__button');
 
-const elementTemplate = document.querySelector('.element-template').content;
-const editTemplate = document.querySelector('.edit-template').content;
-const addTemplate = document.querySelector('.add-template').content;
-const picTemplate = document.querySelector('.pic-template').content;
+// edit variable
+const edit = document.querySelector('.edit');
+const editForm = edit.querySelector('.edit__form');
+const inputName = editForm.querySelector('.edit__input_type_name');
+const inputJob = editForm.querySelector('.edit__input_type_career');
+
+// add variable
+const add = document.querySelector('.add');
+const addForm = add.querySelector('.add__form');
+const titleInput = addForm.querySelector('.add__input_type_title');
+const linkInput = addForm.querySelector('.add__input_type_link');
+
+// pic variable
+const pic = document.querySelector('.pic');
+const picImg = pic.querySelector('.pic__img');
+const picTitle = pic.querySelector('.pic__title');
 
 //  work with profile form
-const showPopup = () => {
+const showPopup = (popup) => {
   popup.classList.remove('popup_closed');
 };
 
-const closePopup = () => {
+const closePopup = (popup) => {
   popup.classList.add('popup_closed');
-  popup.removeChild(popup.lastChild);
 };
 
 // find the form fields in the DOM
@@ -27,32 +36,45 @@ const profileName = body.querySelector('.profile__info-name');
 const profileJob = body.querySelector('.profile__info-career');
 
 // function for show edit profile form and edit profile
-const showProfileForm = () => {
-  const edit = editTemplate.querySelector('.edit__container').cloneNode(true);
-
-  const editCloseBtn = edit.querySelector('.popup__button-closed');
-  editCloseBtn.addEventListener('click', closePopup);
-
-  popup.append(edit);
-  showPopup();
-  const inputName = document.querySelector('.edit__input_type_name');
-  const inputJob = document.querySelector('.edit__input_type_career');
-  const editForm = edit.querySelector('.edit__form');
-
-  function handleProfileFormSubmit(evt) {
-    evt.preventDefault();
-
-    profileName.textContent = inputName.value;
-    profileJob.textContent = inputJob.value;
-
-    closePopup();
-  }
-
-  editForm.addEventListener('submit', handleProfileFormSubmit);
+const showProfilePopup = () => {
+  showPopup(edit);
 
   inputName.value = profileName.textContent;
   inputJob.value = profileJob.textContent;
 };
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+
+  closePopup(edit);
+}
+
+const showAddPopup = () => {
+  showPopup(add);
+
+  inputName.value = '';
+  inputJob.value = '';
+};
+
+const handleAddFormSubmit = (evt) => {
+  evt.preventDefault();
+
+  data.name.unshift(titleInput.value);
+  data.link.unshift(linkInput.value);
+  const element = createCard(data.name[0], data.link[0]);
+
+  elements.prepend(element);
+  titleInput.value = '';
+  linkInput.value = '';
+  closePopup(add);
+};
+
+// add function to each form when they submit
+editForm.addEventListener('submit', handleProfileFormSubmit);
+addForm.addEventListener('submit', handleAddFormSubmit);
 
 //  render card element fields
 const data = {
@@ -76,20 +98,11 @@ const data = {
 
 // function popup picture
 const popupPicture = (evt) => {
-  const pic = picTemplate.querySelector('.pic__container').cloneNode(true);
-
-  const picCloseBtn = pic.querySelector('.popup__button-closed');
-  picCloseBtn.addEventListener('click', closePopup);
-
-  const picImg = pic.querySelector('.pic__img');
-  const picTitle = pic.querySelector('.pic__title');
-
   picImg.src = evt.target.src;
   picTitle.textContent = evt.target.alt;
   picImg.alt = evt.target.alt;
 
-  popup.append(pic);
-  showPopup();
+  showPopup(pic);
 };
 
 // function delete element
@@ -133,39 +146,12 @@ const getCardElement = (data) => {
 
 // call function
 // get card element from data variable
-
 getCardElement(data);
 
-// function for show add form and create element
-
-const showAddForm = () => {
-  const add = addTemplate.querySelector('.add__container').cloneNode(true);
-
-  const addCloseBtn = add.querySelector('.popup__button-closed');
-  addCloseBtn.addEventListener('click', closePopup);
-
-  popup.append(add);
-  showPopup();
-  const titleInput = document.querySelector('.add__input_type_title');
-  const linkInput = document.querySelector('.add__input_type_link');
-  const addForm = document.querySelector('.add__form');
-
-  const handleAddFormSubmit = (evt) => {
-    evt.preventDefault();
-
-    data.name.unshift(titleInput.value);
-    data.link.unshift(linkInput.value);
-    const element = createCard(data.name[0], data.link[0]);
-
-    elements.prepend(element);
-    titleInput.value = '';
-    linkInput.value = '';
-    closePopup();
-  };
-
-  addForm.addEventListener('submit', handleAddFormSubmit);
-};
-
 // connect the handler to the form:
-editBtn.addEventListener('click', showProfileForm);
-addBtn.addEventListener('click', showAddForm);
+editBtn.addEventListener('click', showProfilePopup);
+addBtn.addEventListener('click', showAddPopup);
+closeBtn.forEach((button) => {
+  const popup = button.closest('.popup');
+  button.addEventListener('click', () => closePopup(popup));
+});
